86ceb909e1095790547687c68d2d04b04860434f
Oscar Beaumont <oscar@otbeaumont.me>
2024-08-17 13:21:49 +0800

    :tada: Release v2.0.0-rc.17 w/ Tauri  2.0.0-rc.3

--- src/lib.rs
+++ src/lib.rs
@@ -14,15 +14,15 @@
 //!
 //! ```sh
 //! # Always required
-//! cargo add tauri@=2.0.0-rc.2 specta@=2.0.0-rc.20
+//! cargo add tauri@=2.0.0-rc.3 specta@=2.0.0-rc.20
 //!
 //! # Typescript
 //! cargo add specta-typescript@0.0.7
-//! cargo add tauri-specta@=2.0.0-rc.12 --features derive,typescript
+//! cargo add tauri-specta@=2.0.0-rc.17 --features derive,typescript
 //!
 //! # JSDoc
 //! cargo add specta-jsdoc@0.0.7
-//! cargo add tauri-specta@=2.0.0-rc.12 --features derive,javascript
+//! cargo add tauri-specta@=2.0.0-rc.17 --features derive,javascript
 //! ```
 //!
 //! ## Features
