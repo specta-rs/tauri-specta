8efce75f47f0f330427af54f0e65ff4e67a8ed42
Brian Schiller <bgschiller@gmail.com>
2024-09-05 10:04:57 -0700

    Remove unused TAURI_CHANNEL import
    Depending on your tsconfig, unused imports may be compilation errors.

--- examples/app/src/bindings.ts
+++ examples/app/src/bindings.ts
@@ -82,7 +82,6 @@ export type MyStruct = { some_field: string }
 
 import {
 	invoke as TAURI_INVOKE,
-	Channel as TAURI_CHANNEL,
 } from "@tauri-apps/api/core";
 import * as TAURI_API_EVENT from "@tauri-apps/api/event";
 import { type WebviewWindow as __WebviewWindow__ } from "@tauri-apps/api/webviewWindow";
