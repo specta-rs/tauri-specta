6d2710985ba40d44121eafe9d778298786d80ce4
Oscar Beaumont <oscar@otbeaumont.me>
2026-03-30 21:34:07 +0800

    Specta Tags

--- examples/app/src/main.ts
+++ examples/app/src/main.ts
@@ -1,5 +1,7 @@
+
 import { getCurrentWebview } from "@tauri-apps/api/webview";
 import { commands, events } from "./bindings";
+import { Channel, invoke } from "@tauri-apps/api/core";
 // import { commands, events } from "./bindings-jsdoc.js";
 
 const appWindow = getCurrentWebview();
@@ -31,3 +33,75 @@ window.addEventListener("DOMContentLoaded", () => {
 
 events.emptyEvent.listen((e) => console.log("Global event", e));
 events.emptyEvent(appWindow).listen((e) => console.log("Window event", e));
+
+
+
+invoke("special_types", {
+  input: {
+    //   // TODO: Fix all these to big types
+    //   // TODO: Fix all these types
+    //   // TODO: Date working
+    //   // TODO: Make them `number | BigInt` for arguments
+
+    //   // u128_max: (1n << 128n) - 1n,
+    //   // u128_min: 0n,
+    //   // i128_max: (1n << 127n) - 1n,
+    //   // i128_min: -(1n << 127n),
+    //   // TODO: The types for these are not correct at all
+    //   // bytes: [4, 3, 2, 1],
+    //   // bytes_from_vec: new Uint8Array([4, 3, 2, 1]),
+    //   // TODO: Fix these types as `string | Date`
+    //   // date: new Date(),
+    //   // datetime: new Date(),
+    //
+    // u128_max: (1n << 128n) - 1n,
+    // u128_min: 0n,
+    // i128_max: (1n << 127n) - 1n,
+    // i128_min: -(1n << 127n),
+    // TODO: Don't do this
+    u128_max: 42,
+    u128_min: 42,
+    i128_max: 42,
+    i128_min: 42,
+} }, {
+  reviver: (key, value, context) => {
+   // console.log("DEBUG REVIVER", key, value, context);
+
+   if (
+     context &&
+     typeof value === "number" &&
+     Number.isInteger(value) &&
+     /^-?\d+$/.test(context.source) &&
+     !Number.isSafeInteger(value)
+   ) {
+     // console.log("FOUND BIGINT", context.source);
+     return BigInt(context.source);
+   }
+
+   return value;
+ }
+}).then(([echo, from_rs]) => {
+    console.log("SPECIAL TYPES:", echo, from_rs);
+    console.log(
+      "ECHO ASSERTIONS:",
+      echo.u128_max == (1n << 128n) - 1n,
+      echo.u128_min == 0n,
+      echo.i128_max == (1n << 127n) - 1n,
+      echo.i128_min == -(1n << 127n)
+    );
+    console.log(
+      "FROM_RS ASSERTIONS:",
+      from_rs.u128_max == (1n << 128n) - 1n,
+      from_rs.u128_min == 0n,
+      from_rs.i128_max == (1n << 127n) - 1n,
+      from_rs.i128_min == -(1n << 127n)
+    );
+});
+
+// const channel = new Channel<bigint>();
+// channel.onmessage = (value) => {
+//   console.log("channel bigint:", value);
+//   console.log("is bigint:", typeof value === "bigint");
+//   console.log("matches u128::MAX:", value === (1n << 128n) - 1n);
+// };
+// await commands.specialTypesWChannel(channel);
